60afc6339193a431baf875df9bd2a350473fe33e
JiahaoZhang-Public <jiahao.zhang.public@gmail.com>
2025-01-13 09:48:21 +0800

    version:0.1.0

--- setup.py
+++ setup.py
@@ -2,11 +2,27 @@ from setuptools import setup, find_packages
 
 setup(
     name="tic",
-    version="0.0.0",
+    version="0.1.0",
     author="Jiahao Zhang",
     description=(
-        "Temperal Inference of tumer Cells"
+        "Temporal Inference of Tumor Cells"
     ),
-    packages=find_packages(include=['adapters','adapters.*','core', 'core.*', 'tools', 'tool.*','utils', 'utils.*']),
+    packages=find_packages(include=['adapters', 'adapters.*', 'core', 'core.*', 'tools', 'tools.*', 'utils', 'utils.*']),
     python_requires=">=3.7",
+    install_requires=[
+        "hydra-core>=1.2.0",
+        "umap-learn>=0.5.0",
+    ],
+    extras_require={
+        "submodules": [
+            # Instructions for installing submodules as local editable packages
+            "spacegm @ file://tools/space-gm",
+            "pyslingshot @ file://tools/slingshot",
+        ]
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
 )
